dbd02d0ade9694c642318d7dfff46884052402ff
Hisham C <hishamc18@gmail.com>
2024-12-03 19:46:00 +0530

    get user by id, block user

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -4,57 +4,82 @@ const CustomError = require('../utils/customError')
 const Product = require('../models/productModel')
 
 //Get All users
-exports.getAllUsers = asyncHandler(async (req,res)=>{
-    const users = await User.find({role: "user"}).select('-password')
-    res.status(200).json({success: true, count: users.length, message: "All users fetched", users})
+exports.getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({ role: "user" }).select('-password')
+    res.status(200).json({ success: true, count: users.length, message: "All users fetched", data: users })
 })
 
 //get user by id
-exports.getUserById = asyncHandler()
+exports.getUserById = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const user = await User.findById(id).select('-password')
+    if (!user) {
+        throw new CustomError(`No user found with ${id}`, 404)
+    }
+    res.status(200).json({
+        success: true, data: user
+    })
+})
+
+//Block User
+exports.blockUser = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const user = await User.findById(id)
+
+    if (!user) {
+        throw new CustomError(`No user found with ${id}`, 404)
+    }
+
+    if (user.isBlocked) {
+        throw new CustomError(`User with ${id} is already blocked`, 400)
+    }
+        user.isBlocked = true;
+        await user.save();
+})
 
 // Add new product
 exports.addProduct = asyncHandler(async (req, res) => {
     const {
-      name,
-      price,
-      oldPrice,
-      image,
-      category,
-      seller,
-      stock,
-      description,
-      ingredients,
-      iseDeleted
+        name,
+        price,
+        oldPrice,
+        image,
+        category,
+        seller,
+        stock,
+        description,
+        ingredients,
+        iseDeleted
     } = req.body;
-  
+
     // Validate input
     if (!name || !price || !image || !category || !seller || !stock || !description || !ingredients) {
-      throw new CustomError('All fields are required to add a product', 400);
+        throw new CustomError('All fields are required to add a product', 400);
     }
-  
+
     // Create product
     try {
-    const product = await Product.create({
-      name,
-      price,
-      oldPrice,
-      image,
-      category,
-      seller,
-      stock,
-      description,
-      ingredients,
-      iseDeleted,
-    });
-  
-    res.status(201).json({
-      success: true,
-      message: 'Product added successfully',
-      product,
-    });
-} catch (error){
-    if(error.code = 11000){
-        throw new CustomError('Product name must be unique, A product with same name already exist', 400);
+        const product = await Product.create({
+            name,
+            price,
+            oldPrice,
+            image,
+            category,
+            seller,
+            stock,
+            description,
+            ingredients,
+            iseDeleted,
+        });
+
+        res.status(201).json({
+            success: true,
+            message: 'Product added successfully',
+            product,
+        });
+    } catch (error) {
+        if (error.code = 11000) {
+            throw new CustomError('Product name must be unique, A product with same name already exist', 400);
+        }
     }
-}
-  });
+});
--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -4,6 +4,8 @@ const {isAdmin, protect} = require('../middlewares/authMiddleware')
 const adminController = require('../controllers/adminController')
 
 router.get('/users', protect, isAdmin, adminController.getAllUsers)
+router.get('/users/:id', protect, isAdmin, adminController.getUserById);
+router.put('/users/:id', protect, isAdmin, adminController.blockUser);
 router.post('/add', protect, isAdmin, adminController.addProduct);
 
 
